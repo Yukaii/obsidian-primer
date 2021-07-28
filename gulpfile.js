620ff5222b9f4a463df9196c40610ef7add06105
Yukai Huang <yukaihuangtw@gmail.com>
2021-07-28 21:14:51 +0800

    Gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')(require('sass'))
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const clean = require('gulp-clean')
 
 function build() {
   return gulp
-    .src('src/**/*.scss')
+    .src('src/theme.scss')
     .pipe(
       sass
         .sync({
           includePaths: ['node_modules'],
+          sourceMapEmbed: true,
+          sourceMap: true,
         })
         .on('error', sass.logError)
     )
     .pipe(gulp.dest('./dist'))
 }
 
-exports.build = build
+exports.build = gulp.series(cleanTask, build)
 
 exports.watch = function () {
   return gulp.watch('src/**/*.scss', { ignoreInitial: false }, build)
 }
 
 exports.default = build
+
+function cleanTask() {
+  return gulp.src('dist').pipe(clean())
+}
+
+exports.clean = cleanTask
